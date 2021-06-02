3ee9d3b2aa0b82de495129f7b013945b08f2ff8e
annyrviloria <annyrviloria@g.ucla.edu>
2021-06-01 19:59:01 -0700

    map edits

--- Rough_Draft/js/init.js
+++ Rough_Draft/js/init.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([30.63188332081661, -107.4562838930214], 5);
+const map = L.map('map').setView([30.63188332081661, -107.4562838930214], 4);
 
 const url = "https://spreadsheets.google.com/feeds/list/1RlYgkgQCWUYNnwwPTRN4qrrIzkZ_fv6zJYtOFoK-9f4/ocya9of/public/values?alt=json"
 
@@ -23,23 +23,31 @@ fetch(url)
 let newReport = L.featureGroup();
 let otherReport = L.featureGroup();
 
-const boundaryLayer ="../data/map.geojson"
+const boundaryLayer ="./data/map.geojson"
 let boundary;
 let ptsWithin;
 let collected;
+let boundaryGeo;
 let allPoints = [];
 function onEachFeature(feature, layer) {
-    console.log(feature.properties)
+    // console.log(feature.properties)
+
     if (feature.properties.values) {
         let count = feature.properties.values.length
         console.log(count)
         let text = count.toString()
-        layer.bindPopup(text);
+        
+        layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight
+                // click: layer.bindPopup(text)
+            });
+            ;
     }
 }
 
 function getStyles(data){
-        console.log(data)
+        // console.log(data)
         let myStyle = {
             "color": "#ff7800",
             "weight": 1,
@@ -62,21 +70,21 @@ function getStyles(data){
                     boundary = data
     
                     // run the turf collect geoprocessing
-                    collected = turf.collect(boundary, thePoints, 'speakEnglish', 'values');
+                    collected = turf.collect(boundary, thePoints, 'reportType', 'values');
                     // just for fun, you can make buffers instead of the collect too:
                     // collected = turf.buffer(thePoints, 50,{units:'miles'});
                     console.log(collected.features)
     
                     // here is the geoJson of the `collected` result:
-                    L.geoJson(collected,{onEachFeature: onEachFeature,style:function(feature)
+                    boundaryGeo = L.geoJson(collected,{onEachFeature: onEachFeature,style:function(feature)
                     {
-                        console.log(feature)
+                        // console.log(feature)
                         if (feature.properties.values.length > 0) {
                             return {color: "#ff0000",stroke: false};
                         }
                         else{
                             // make the polygon gray and blend in with basemap if it doesn't have any values
-                            return{opacity:0,color = "#efefef" }
+                            return{opacity:0,color: "#efefef" };
                         }
                     }
                     // add the geojson to the map
@@ -96,18 +104,22 @@ let countOtherReports = 0
 // color each state based on the count of the data
 
 function addMarker(data){
-        console.log(data.reportedtowho)
+        console.log(data.whowasthisincidentreportedto)
         let reportType
         // this logic is for interactivity and the actual data
-        if(data.reportedtowho == "I did not report this incident to any authorities"){
+        if(data.whowasthisincidentreportedto == "I did not report this incident to any authorities"){
                 //
                 countNewReports += 1
                 reportType = "new"
+                newReport.addLayer(L.circleMarker([data.lat,data.lng]))
+
         }
         else{
                 //
                 countOtherReports += 1
                 reportType = "other"
+                otherReport.addLayer(L.circleMarker([data.lat,data.lng]))
+
         }
         let thisMarker = {
                 "lat":data.lat,
@@ -116,9 +128,12 @@ function addMarker(data){
                 "timestamp":data.timestamp,
                 
         }
-
+        let theTime = data.timestamp
+        // create the turfJS point
+        let thisPoint = turf.point([Number(data.lng),Number(data.lat)],{reportType,theTime})
+        // put all the turfJS points into `allPoints`
+        allPoints.push(thisPoint)
 }
-        // console.log(data)
 
 // refer to this: https://github.com/rachan2023/21S-191A-Against-Asian-Hate/blob/main/Final%20project/js/init.js
 
@@ -178,11 +193,17 @@ function formatData(theData){
         console.log(formattedData)
         console.log('boundary')
         console.log(boundary)
-        let totalResults = formattedData.forEach(addMarker)
-        let firstReports = totalResults[0] //find out what this is
-        let otherReports = totalResults[1]
-        selfReport.addTo(map)
-        advocateReport.addTo(map)
+        formattedData.forEach(addMarker)
+        console.log('countNewReports')
+        console.log(countNewReports)
+        console.log('countOtherReports')
+        console.log(countOtherReports)
+        // let firstReports = totalResults[0] //find out what this is
+        // console.log('firstReports')
+        // console.log(firstReports)
+        // let otherReports = totalResults[1]
+        newReport.addTo(map)
+        otherReport.addTo(map)
         let allLayers = L.featureGroup([newReport,otherReport]);
 
         // step 1: turn allPoints into a turf.js featureCollection
@@ -194,12 +215,68 @@ function formatData(theData){
         console.log('boundary')
         console.log(boundary)
         map.fitBounds(allLayers.getBounds()); 
+    }
 
 let layers = {
 	"Events <strong>not reported</strong> to the authorities": newReport,
 	"Events reported to an authority": otherReport
 }
 
-L.control.layers(null,layers,{collapsed:false}).addTo(map)
+//L.control.layers(null,layers,{collapsed:false}).addTo(map)
 
 collected.features.properties.values
+
+/////////////////// UI STUFF
+
+var info = L.control();
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+info.addTo(map);
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+        '<b></b><br /> people / mi<sup>2</sup>'
+        : 'Hover over a state');
+};
+
+
+
+function highlightFeature(e) {
+    var layer = e.target;
+    console.log('layer')
+    console.log(layer.feature.properties.values)
+    let boundaryProperties = layer.feature.properties
+    let divToUpdate = document.getElementById("box1")
+    console.log('divToUpdate')
+    console.log(divToUpdate)
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    updateContentsPanel(divToUpdate,boundaryProperties)
+}
+    
+function resetHighlight(e) {
+    boundaryGeo.resetStyle(e.target);
+}
+
+function updateContentsPanel(target,boundaryValues){
+    // use console.log to see what properties can be accessed
+    console.log(boundaryValues)
+    let stateName = boundaryValues.NAME//
+
+    
+    let count = boundaryValues.values.length
+    console.log('count')
+    
+    target.innerHTML = `<h2>State: ${boundaryValues.NAME}</h2><h3>${count}</h3>`
+}
